63643d7c44059e27be8040b6bec1a572cec560ec
Barbara Sousa <barbarapsousa@hotmail.com>
2021-09-14 04:59:38 -0300

    Criando Primeira Class

---
+++ index.js
@@ -0,0 +1,25 @@
+//declarando class
+class cliente {
+  nome
+  cpf
+  agencia
+  saldo
+}
+
+const cliente1 = new cliente() //criando um novo obj
+
+//atributos obj
+cliente1.nome = 'Paulo'
+cliente1.cpf = 15976398634
+cliente1.agencia = '0298'
+cliente1.saldo = 0
+
+const cliente2 = new cliente() ////criando um novo obj
+
+//atributos obj
+cliente2.nome = 'Paula'
+cliente2.cpf = 15696398634
+cliente2.agencia = '0298'
+cliente2.saldo = 0
+
+console.log(cliente1, cliente2)
