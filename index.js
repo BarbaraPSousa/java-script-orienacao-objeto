3b490a393e3a04e9091bea38ef68de200aeb9412
Barbara Sousa <barbarapsousa@hotmail.com>
2021-09-14 05:26:03 -0300

    Comportamentos da Class

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 //declarando class
-class cliente {
+class Cliente {
   nome
   cpf
-  agencia
-  saldo
+  
 }
 
-const cliente1 = new cliente() //criando um novo obj
+class ContaCorrente{
+  agencia
+  saldo
 
+  sacar(valor){
+    if(this.saldo >= valor){
+      this.saldo -= valor;
+    }
+  }
+}
+
+const cliente1 = new Cliente() //criando um novo obj
 //atributos obj
 cliente1.nome = 'Paulo'
 cliente1.cpf = 15976398634
-cliente1.agencia = '0298'
-cliente1.saldo = 0
 
-const cliente2 = new cliente() ////criando um novo obj
 
+const cliente2 = new Cliente() ////criando um novo obj
 //atributos obj
 cliente2.nome = 'Paula'
 cliente2.cpf = 15696398634
-cliente2.agencia = '0298'
-cliente2.saldo = 0
 
-console.log(cliente1, cliente2)
+
+const contaCorrentePaulo = new ContaCorrente()
+contaCorrentePaulo.agencia = 1001
+contaCorrentePaulo.saldo = 0;
+console.log(contaCorrentePaulo.saldo); // esperado saldo 0
+contaCorrentePaulo.saldo = 100;
+console.log(contaCorrentePaulo.saldo); // // esperado saldo 100
+contaCorrentePaulo.sacar(50);
+console.log(contaCorrentePaulo.saldo); // esperado saldo 50-100 = 50
+contaCorrentePaulo.sacar(200);
+console.log(contaCorrentePaulo.saldo); // esperado saldo 200-100 = -100, não permitido
+
+
+
+//imprimindo no console obj
+console.log(cliente1);
+console.log(cliente2);
